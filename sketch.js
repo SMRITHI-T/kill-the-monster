441f9aca6857e58af487296c1eaaee72456e785e
SMRITHI-T <74653719+SMRITHI-T@users.noreply.github.com>
2021-03-20 17:10:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+function preload() {
+//preload the images here
+
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  engine = Engine.create();
+  world = engine.world;
+ground=new Ground(1500,750,300,20);
+  //row 1
+box1=new Box(500,750);
+box2=new Box(500,700);
+box3=new Box(500,650);
+box4=new Box(500,600);
+box5=new Box(500,550);
+box6=new Box(500,500);
+box7=new Box(500,450);
+box8=new Box(500,400);
+box9=new Box(500,350);
+box10=new Box(500,300);
+//row 2
+box11=new Box(600,750);
+box12=new Box(600,700);
+box13=new Box(600,650);
+box14=new Box(600,600);
+box15=new Box(600,550);
+box16=new Box(600,500);
+box17=new Box(600,450);
+box18=new Box(600,400);
+box19=new Box(600,350);
+//row 3
+box20=new Box(700,750);
+box21=new Box(700,700);
+box22=new Box(700,650);
+box23=new Box(700,600);
+box24=new Box(700,550);
+box25=new Box(700,500);
+box26=new Box(700,450);
+box27=new Box(700,400);
+//row 4
+box28=new Box(800,750);
+box29=new Box(800,700);
+box30=new Box(800,650);
+box31=new Box(800,600);
+box32=new Box(800,550);
+box33=new Box(800,500);
+box34=new Box(800,450);
+
+
+
+
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+ground.display();
+
+box1.display();
+box2.display();
+box3.display();
+box4.display();
+box5.display();
+box6.display();
+box7.display();
+box8.display();
+box9.display();
+box10.display();
+box11.display();
+box12.display();
+box13.display();
+box14.display();
+box15.display();
+box16.display();
+box17.display();
+box18.display();
+box19.display();
+box20.display();
+box21.display();
+box22.display();
+box23.display();
+box24.display();
+box25.display();
+box26.display();
+box27.display();
+box28.display();
+box29.display();
+box30.display();
+box31.display();
+box32.display();
+box33.display();
+box34.display();
+}
